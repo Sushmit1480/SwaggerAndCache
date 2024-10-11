e20951e89e7b8240918886b2ba3aa039d57eaa4f
Sushmit Patil <67361895+Sushmit1480@users.noreply.github.com>
2024-10-11 16:41:41 +0530

    Added image Field

--- src/main/java/com/swaggerExample/swaggerExample/model/Employee.java
+++ src/main/java/com/swaggerExample/swaggerExample/model/Employee.java
@@ -1,11 +1,12 @@
 package com.swaggerExample.swaggerExample.model;
 
 import org.springframework.stereotype.Component;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,64 +20,99 @@ public class Employee {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
+	@Column(name = "first_name", nullable = false)
 	private String firstName;
+	
+	@Column(name = "last_name", nullable = false)
 	private String lastName;
+	
+	@Column(name = "mobile", nullable = false)
 	private String mobile;
+	
+	@Column(name = "email", nullable = false, unique = true)
 	private String email;
+	
+	@Column(name = "salary", nullable = false)
 	private double salary;
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getMobile() {
-		return mobile;
-	}
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public double getSalary() {
-		return salary;
-	}
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-	public Employee(Integer id, String firstName, String lastName, String mobile, String email, double salary) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.mobile = mobile;
-		this.email = email;
-		this.salary = salary;
-	}
+	
+	@Lob
+	@Column(name = "photo", columnDefinition = "BLOB")
+	private byte[] photo;
+
 	public Employee() {
 		super();
 	}
+
+	public Employee(String firstName, String lastName, String mobile, String email, double salary,
+			byte[] photo) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.mobile = mobile;
+		this.email = email;
+		this.salary = salary;
+		this.photo = photo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", mobile=" + mobile
 				+ ", email=" + email + ", salary=" + salary + "]";
 	}
-	
-	
 }
